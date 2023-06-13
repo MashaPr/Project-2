f632cb974dea0f9bd6128c314338d7e7840d4b33
mashapr03@outlook.com <mashapr03@outlook.com>
2023-06-14 01:03:03 +0300

    please work

---
+++ src/components/preloader.jsx
@@ -0,0 +1,8 @@
+function Preloader() {
+    return <div className="progress">
+        <h3>Идёт агрузка...</h3>
+        <div className="indeterminate"></div>
+    </div>
+}
+
+export { Preloader };
